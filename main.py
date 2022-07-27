d51c33d50ff81ad4288acb7d2ee61ff7e8edc903
toxidxd <toxid13@yandex.ru>
2022-07-28 02:04:13 +0300

    Def to download from list

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import pytube
 import os
 
-url = input("Input video url: ")
 
-if not os.path.exists("Downloads"):
-    os.mkdir("Downloads")
+def dl_from_list():
+    links = []
 
-youtube = pytube.YouTube(url)
+    with open("links.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            links.append(line.strip())
 
-video = youtube.streams.get_highest_resolution()
-# video = youtube.streams.get_by_resolution(resolution="720p")
-video.download("Downloads")
+    if not os.path.exists("Downloads"):
+        os.mkdir("Downloads")
+
+    for link in links:
+        youtube = pytube.YouTube(link)
+        video = youtube.streams.get_highest_resolution()
+        # video = youtube.streams.get_by_resolution(resolution="720p")
+        video.download("Downloads")
+
+    return print("Downloaded")
+
+
+def main():
+    dl_from_list()
+
+
+if __name__ == "__main__":
+    main()
